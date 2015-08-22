0cacba8632948bf5355182b040c5604d0b616f88
arminhammer <arminhammer@gmail.com>
2015-08-22 15:41:32 -0400

    Adding morgan logging to server.

--- server/server.js
+++ server/server.js
@@ -3,19 +3,23 @@
  */
 var express = require("express");
 var server = express();
+var morgan = require('morgan');
 
 var port = process.env.PAPERDUCKY_PORT || 3000;
-var environment = process.env.PAPERDUCKY_ENV || 'development';
+var env = process.env.PAPERDUCKY_ENV || 'development';
+
+//Set up access logging
+if (app.get('env') == 'production') {
+  app.use(morgan('common', { skip: function(req, res) { return res.statusCode < 400 }, stream: __dirname + '/../access.log' }));
+} else {
+  app.use(morgan('dev'));
+}
 
 server.get("/test", function (req, res) {
   res.send("Well, that worked, just fine.");
 });
 
-server.use(express.static('./dist'));
-
-server.all('/*', function(req, res) {
-  res.sendFile('index.html', { root: 'dist' });
-});
+server.use('/dashboard', express.static('./dist'));
 
 server.listen(port);
 console.log('Server listening on port',port);
