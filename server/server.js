17e33e5a14cd0fe3efc4ea71fc65dbfb8a76d39d
arminhammer <arminhammer@gmail.com>
2015-08-24 21:48:03 -0400

    Added initScrapers method, along with sample scraper filers.

--- server/server.js
+++ server/server.js
@@ -4,19 +4,42 @@
 var express = require("express");
 var server = express();
 var morgan = require('morgan');
+var fs = require('fs');
+var path = require('path');
 
 var port = process.env.PAPERDUCKY_PORT || 3000;
 var env = process.env.PAPERDUCKY_ENV || 'development';
+var scraperDir = path.resolve(__dirname, 'scrapers');
+
+var scrapers = {};
 
 //Set up access logging
-if (app.get('env') == 'production') {
-  app.use(morgan('common', { skip: function(req, res) { return res.statusCode < 400 }, stream: __dirname + '/../access.log' }));
+if (env === 'production') {
+  server.use(morgan('common', { skip: function(req, res) { return res.statusCode < 400 }, stream: __dirname + '/../access.log' }));
 } else {
-  app.use(morgan('dev'));
+  server.use(morgan('dev'));
 }
 
-server.get("/test", function (req, res) {
-  res.send("Well, that worked, just fine.");
+var initScrapers = function(cb) {
+  console.log('Initializing scrapers.');
+  console.log(scraperDir);
+  var files = fs.readdirSync(scraperDir);
+  files.forEach(function(file) {
+    var data = fs.readFileSync(path.resolve(scraperDir,file), 'utf8');
+
+    scrapers[file.substr(0, file.indexOf('.json'))] = data;
+  });
+
+  cb();
+};
+
+initScrapers(function() {
+  console.log(scrapers);
+  console.log('Scrapers initialized.');
+});
+
+server.get("/list", function (req, res) {
+  res.json(scrapers);
 });
 
 server.use('/dashboard', express.static('./dist'));
