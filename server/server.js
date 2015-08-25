0829e3b91bc6c6ce1c52cc526685e7ecd8da77f1
arminhammer <arminhammer@gmail.com>
2015-08-24 22:51:28 -0400

    Adding rubbertiger api.

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@
  * Created by arminhammer on 8/20/15.
  */
 var express = require("express");
-var server = express();
 var morgan = require('morgan');
 var fs = require('fs');
 var path = require('path');
 var rubbertiger = require('rubbertiger');
+var bodyParser = require('body-parser');
 
 var port = process.env.PAPERDUCKY_PORT || 3000;
 var env = process.env.PAPERDUCKY_ENV || 'development';
@@ -14,12 +14,16 @@ var scraperDir = path.resolve(__dirname, 'scrapers');
 
 var scrapers = {};
 
+var server = express();
+
 //Set up access logging
 if (env === 'production') {
   server.use(morgan('common', { skip: function(req, res) { return res.statusCode < 400 }, stream: __dirname + '/../access.log' }));
 } else {
   server.use(morgan('dev'));
 }
+
+server.use(bodyParser.json()); // for parsing application/json
 
 var initScrapers = function(cb) {
   console.log('Initializing scrapers.');
@@ -48,7 +52,15 @@ server.get("/api/describe/:name", function (req, res) {
 });
 
 server.post("/api/exec/:name", function (req, res) {
-  res.send(JSON.stringify(scrapers[req.params.name],null,2));
+  console.log(req.body);
+
+  var options;
+  req.body.options ? options = req.body.options : options = { driver: 'osmosis' };
+
+  rubbertiger.scrape(scrapers[req.params.name], options)
+    .then(function(data) {
+      res.send(data);
+    });
 });
 
 server.use('/dashboard', express.static('./dist'));
